d9d705557ceb3f15d346e3ba5d67dc55f8985adb
Guilherme Augusto <guilherme.francais@gmail.com>
2018-11-09 11:47:30 -0200

    Creates a recursive implementation to knapsack
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Co-authored-by: Icaro Oliveira <icarooliv2@gmail.com>

--- knapsack/table.py
+++ knapsack/table.py
@@ -62,3 +62,31 @@ class GenerateTable():
         matrix[row][column] = str(response)
 
         return matrix
+
+
+def recursive_knapsack(i, w):
+
+    if i < 0 or w <= 0:
+        return 0
+
+    if memoization_matrix[i][w] != -1:
+        return memoization_matrix[i][w]
+
+    if W[i] > w:
+        memoization_matrix[i][w] = recursive_knapsack(i - 1, w)
+        return memoization_matrix[i][w]
+
+    not_take = recursive_knapsack(i - 1, w)
+    take = recursive_knapsack(i - 1, w - W[i]) + V[i]
+
+    if take > not_take:
+        memoization_matrix[i][w] = take
+        visual_matrix[i][w] = str(memoization_matrix[i][w])
+        taken[i][w] = True
+    else:
+        memoization_matrix[i][w] = not_take
+        visual_matrix[i][w] = str(memoization_matrix[i][w])
+
+    # table.create_knapsack_matrix(visual_matrix, headers)
+
+    return memoization_matrix[i][w]
