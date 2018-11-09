af725c8434e772208ae5105b65db19fd64d1457b
Guilherme Augusto <guilherme.francais@gmail.com>
2018-11-09 11:48:39 -0200

    Creates a iterative knapsack method
    Co-authored-by: Guilherme Augusto <guilherme.francais@gmail.com>
    Co-authored-by: Icaro Oliveira <icarooliv2@gmail.com>

--- knapsack/table.py
+++ knapsack/table.py
@@ -90,3 +90,33 @@ def recursive_knapsack(i, w):
     # table.create_knapsack_matrix(visual_matrix, headers)
 
     return memoization_matrix[i][w]
+
+def iterative_knapsack(quantity, weight, iterative_matrix):
+    i = 1
+
+    for i in range(weight):
+        iterative_matrix[0][i+1] = 0
+        visual_iterative_matrix[0][i+1] = str(iterative_matrix[0][i+1])
+
+    i = 1
+
+    for i in range(quantity):
+        iterative_matrix[i][1] = 0
+        visual_iterative_matrix[i][1] = str(iterative_matrix[i][1])
+
+        w = 2
+
+        for w in range(2, weight+1):
+            if W[i] <= w:
+                iterative_matrix[i][w] = max(
+                    V[i] + iterative_matrix[i-1][w - W[i]],
+                    iterative_matrix[i-1][w]
+                )
+
+                visual_iterative_matrix[i][w] = str(iterative_matrix[i][w])
+
+            else:
+                iterative_matrix[i][w] = iterative_matrix[i-1][w]
+                visual_iterative_matrix[i][w] = str(iterative_matrix[i][w])
+
+        # table.create_knapsack_matrix(visual_iterative_matrix, headers)
